1a951ff9432c7322a0faade744e6467aac258e0c
Alan6681 <alanaboamaegbe@gmail.com>
2026-02-21 11:17:03 +0100

    tweak

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173", 
-        "https://audio-lens-v1.vercel.app/",
+        "https://audio-lens-v1.vercel.app",
         "http://localhost:3000" 
     ], 
     allow_credentials=True,
